c8a09f5b3174cf60d8c950332c17c5856393348e
aengussong <oleg.durasov.2016@gmail.com>
2020-05-02 22:47:15 +0300

    update request test

--- app/src/test/java/com/aengussong/standalonenetworkrequest/RequestControllerTest.kt
+++ app/src/test/java/com/aengussong/standalonenetworkrequest/RequestControllerTest.kt
@@ -1,8 +1,12 @@
 package com.aengussong.standalonenetworkrequest
 
+import android.os.Handler
+import org.awaitility.Awaitility.await
+import org.awaitility.kotlin.untilNotNull
 import org.junit.Assert
 import org.junit.Test
 import java.net.URL
+import java.util.concurrent.TimeUnit
 
 
 class RequestControllerTest {
@@ -10,18 +14,33 @@ class RequestControllerTest {
     private val requestController = RequestController()
 
     @Test
-    fun `make request - should return success`(){
+    fun `make get request - should return success`() {
         val username = "aengussong"
         val api = "https://api.github.com/users/$username/repos"
         val url = URL(api)
+        var response: ReposResponse? = null
 
-        val response:ReposResponse = requestController.makeRequest(url)
+        requestController.makeRequest<ReposResponse>(RequestMethod.GET, url){resp ->
+            response = resp
+        }
 
+        await().atMost(requestController.TIMEOUT, TimeUnit.MILLISECONDS)
+            .untilNotNull<ReposResponse>(response)
         Assert.assertTrue(response.isSuccessful)
     }
 
     @Test
-    fun `make request with no network - should throw error`(){
+    fun `make post request - should return success`() {
+        Assert.fail()
+    }
+
+    @Test
+    fun `make put request - should return success`() {
+        Assert.fail()
+    }
+
+    @Test
+    fun `make request with no network - should throw error`() {
         Assert.fail("not implemented")
     }
 }
