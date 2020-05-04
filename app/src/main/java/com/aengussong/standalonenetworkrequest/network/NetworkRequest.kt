ca5f9559d11ee5cc3afb4732eb162af39f29a6ba
aengussong <oleg.durasov.2016@gmail.com>
2020-05-04 13:17:13 +0300

    add network request documentation

--- app/src/main/java/com/aengussong/standalonenetworkrequest/network/NetworkRequest.kt
+++ app/src/main/java/com/aengussong/standalonenetworkrequest/network/NetworkRequest.kt
@@ -1,18 +1,34 @@
 package com.aengussong.standalonenetworkrequest.network
 
 import android.os.Handler
-import com.aengussong.standalonenetworkrequest.model.RequestResponse
+import com.aengussong.standalonenetworkrequest.model.NetworkResponse
+import com.aengussong.standalonenetworkrequest.parser.Parser
 import com.aengussong.standalonenetworkrequest.utils.Utils
 import java.lang.ref.WeakReference
 import java.net.URL
 import javax.net.ssl.HttpsURLConnection
+import kotlin.reflect.KClass
 
-class NetworkRequest<T>(
+/**
+ * Performs network request with specified connection [timeout], [requestMethod] on specified [url].
+ * After successful connect url response will be parsed to [klass]
+ *
+ * @param timeout - request connection timeout
+ * @param requestMethod - HTTP request method
+ * @param klass - request response will be tried to be deserialized to this [KClass]
+ * @param handler - adds [callback] to the message queue, should be declared with the looper of
+ * thread, on which [callback] should be executed
+ * @param parser - request response deserializer, will try to deserialize response to [klass]
+ * @param callback - deliver request's deserialized response
+ * */
+class NetworkRequest<T : Any>(
     private val timeout: Int,
     private val requestMethod: RequestMethod,
     private val url: URL,
+    private val klass: KClass<T>,
     private val handler: Handler,
-    private val callback: WeakReference<(RequestResponse<T>) -> Unit>
+    private val parser: Parser,
+    private val callback: WeakReference<(NetworkResponse<T>) -> Unit>
 ) : Runnable {
 
     override fun run() {
@@ -21,11 +37,18 @@ class NetworkRequest<T>(
             connection.connectTimeout = timeout
             connection.requestMethod = requestMethod.name
             connection.connect()
-            val data = Utils.readStream(connection.inputStream)
-            val parsedData: T? = Utils.parseString(data ?: "")
-            val isSuccessful = true
-            val responseCode = 200
-            val response = RequestResponse<T>(isSuccessful, responseCode, parsedData)
+
+            val responseCode = connection.responseCode
+            val isSuccessful = responseCode == HttpsURLConnection.HTTP_OK
+            var parsedData: T? = null
+
+            //read response if connection was successful
+            if (isSuccessful) {
+                val data = Utils.readStream(connection.inputStream)
+                parsedData = parser.parse(data ?: "", klass)
+            }
+
+            val response = NetworkResponse<T>(isSuccessful, responseCode, parsedData)
             handler.post { callback.get()?.invoke(response) }
         } finally {
             connection.inputStream.close()
